4ebf1e3238911f6f44ca7e649a9440729f3ea7b4
Andriy Sydorenko <sidorenkoandrij217@gmail.com>
2023-07-24 17:40:08 +0300

    Update settings

--- restaurant_service/settings.py
+++ restaurant_service/settings.py
@@ -9,7 +9,7 @@ https://docs.djangoproject.com/en/4.2/topics/settings/
 For the full list of settings and their values, see
 https://docs.djangoproject.com/en/4.2/ref/settings/
 """
-
+import os
 from pathlib import Path
 
 # Build paths inside the project like this: BASE_DIR / 'subdir'.
@@ -20,12 +20,12 @@ BASE_DIR = Path(__file__).resolve().parent.parent
 # See https://docs.djangoproject.com/en/4.2/howto/deployment/checklist/
 
 # SECURITY WARNING: keep the secret key used in production secret!
-SECRET_KEY = "django-insecure-p+pxnvpnh*3d)!5e9sv@#g@$k#b#p9ko@$qq7iqh(2)($-7-qf"
+SECRET_KEY = os.environ.get("DJANGO_SECRET_KEY", "django-insecure-p+pxnvpnh*3d)!5e9sv@#g@$k#b#p9ko@$qq7iqh(2)($-7-qf")
 
 # SECURITY WARNING: don't run with debug turned on in production!
-DEBUG = True
+DEBUG = os.environ.get("DJANGO_DEBUG", "") != "False"
 
-ALLOWED_HOSTS = []
+ALLOWED_HOSTS = ["127.0.0.1"]
 
 INTERNAL_IPS = [
     "127.0.0.1",
