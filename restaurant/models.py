eef66bcbf6b96a4e6f0bba849a4cd68cace7e8b7
Andriy Sydorenko <sidorenkoandrij217@gmail.com>
2023-07-25 17:21:20 +0300

    set default value for years_of_experience in Cook model

--- restaurant/models.py
+++ restaurant/models.py
@@ -20,7 +20,7 @@ class DishType(models.Model):
 
 
 class Cook(AbstractUser):
-    years_of_experience = models.IntegerField()
+    years_of_experience = models.IntegerField(default=0)
 
     class Meta:
         ordering = ["username"]
