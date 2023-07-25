6948fa2b43cef10ee6a4a4a17012ca301c9d745e
Andriy Sydorenko <sidorenkoandrij217@gmail.com>
2023-07-25 17:25:38 +0300

    deleted postgres database config in settings.py

--- restaurant_service/settings.py
+++ restaurant_service/settings.py
@@ -91,19 +91,7 @@ WSGI_APPLICATION = "restaurant_service.wsgi.application"
 # Database
 # https://docs.djangoproject.com/en/4.2/ref/settings/#databases
 
-# database settings for production
-# DATABASES = {
-#     'default': {
-#         'ENGINE': 'django.db.backends.postgresql',
-#         'NAME': os.getenv('POSTGRES_DB'),
-#         'USER': os.getenv('POSTGRES_USER'),
-#         'PASSWORD': os.getenv('POSTGRES_PASSWORD'),
-#         'HOST': os.getenv('POSTGRES_HOST'),
-#     }
-# }
 
-
-# for local usage:
 DATABASES = {
     'default': {
         'ENGINE': 'django.db.backends.sqlite3',
